7fc0a59fcf7c9476ece5e5ed6eb7ad8d69912006
kimja01 <kimja7045@naver.com>
2022-04-24 15:31:31 +0900

    페이스북버튼 추가

--- src/pages/auth/LoginPage.js
+++ src/pages/auth/LoginPage.js
@@ -1,7 +1,8 @@
 import React from 'react';
-import { Text, TouchableOpacity } from 'react-native';
+import { Image, Text, TouchableOpacity, StyleSheet } from 'react-native';
 import { CenterContainer } from '../../components/Container';
 import { useNavigation } from '@react-navigation/native';
+// import {AccessToken, Profile, LoginManager} from 'react-native-fbsdk-next';
 
 const LoginPage = (props) => {
   const page = useNavigation();
@@ -9,6 +10,29 @@ const LoginPage = (props) => {
   const onLoginComplete = () => {
     // page.navigate('Home')u;
     page.push('Main');
+  };
+
+  // Ⅰ.페이스북로그인 클릭시 호출되는 함수
+  const facebookLogin = () => {
+    // LoginManager.logInWithPermissions(['public_profile']).then(
+    //     function (result) {
+    //         if (result.isCancelled) {
+    //             console.log('Login cancelled');
+    //         } else {
+    //             console.log(
+    //                 'Login success with permissions: ' +
+    //                 result.grantedPermissions.toString(),
+    //             );
+    //             AccessToken.getCurrentAccessToken().then(data => {
+    //                 console.log(`\n\naccessToken: ${data.accessToken.toString()}\n\n`);
+    //                 getCurrentFacebookProfile();
+    //             });
+    //         }
+    //     },
+    //     function (error) {
+    //         console.log('Login fail with error: ' + error);
+    //     },
+    // );
   };
 
   return (
@@ -27,8 +51,35 @@ const LoginPage = (props) => {
       >
         <Text style={{ color: 'white' }}>메인화면으로 이동</Text>
       </TouchableOpacity>
+
+      <TouchableOpacity style={styles.facebookLoginBtn} onPress={facebookLogin}>
+        <Image
+          style={styles.facebookIcon}
+          source={require('../../assets/facebook.png')}
+        />
+        <Text style={{ fontSize: 18, color: '#fff', marginLeft: 24 }}>
+          페이스북 로그인
+        </Text>
+      </TouchableOpacity>
     </CenterContainer>
   );
 };
 
 export default LoginPage;
+
+const styles = StyleSheet.create({
+  facebookLoginBtn: {
+    backgroundColor: '#3E65B4',
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    paddingVertical: 14,
+    borderRadius: 8,
+    width: '60%',
+    marginTop: 16,
+  },
+  facebookIcon: {
+    width: 10,
+    height: 20,
+  },
+});
